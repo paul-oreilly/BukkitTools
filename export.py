5dfe341405772c4e9ac15a05deba99d70b207d5c
Paul O'Reilly <paul.oreilly@digime.co.nz>
2012-09-13 05:04:08 +1200

    Improved utilities for making local packages from this library

---
+++ export.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+from bin import refactorTool
+import local_settings
+
+if __name__ == "__main__":
+    refactorTool.refactor_as_export()
---
+++ import.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+from bin import refactorTool
+import local_settings
+
+
+if __name__ == "__main__":
+    refactorTool.refactor_as_import()
